8e85ea395e0aa3d3d06bb6d0193b7de5e7069de5
nfandre <andrelive.05@hotmail.com>
2020-10-14 11:10:13 -0300

    aula 11 excecoes

--- Aula11 - excecoes/exer2/src/exer2/Main.java
+++ Aula11 - excecoes/exer2/src/exer2/Main.java
@@ -1,34 +1,41 @@
 package exer2;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		FileWriter arquivo1 = null;
-		FileWriter arquivo2 = null;
 
-		try {
-			arquivo1 = new FileWriter(new File("arquivo1.txt"));
-			arquivo2 = new FileWriter(new File("arquivo2.txt"));
-
+		try (FileWriter arquivo1 = new FileWriter(new File("arquivo1.txt"));
+				FileWriter arquivo2 = new FileWriter(new File("arquivo2.txt"))
+				){
 			arquivo1.write("Andre");
 			arquivo2.write("Nascimento de Freitas");
-			
-			a
+
 
 		} catch (FileNotFoundException e) {
 		    e.printStackTrace();
-		} finally {
-		    if (arquivo1 != null) {
-		    	arquivo1.close();
-		    }
-		}
+		} 
+		
+		readFile();
 
+	}
+	
+	public static void readFile() {
+		try (BufferedReader br = new BufferedReader(new FileReader("arquivo1.txt"));
+				BufferedReader br2 = new BufferedReader(new FileReader("arquivo2.txt"))){
+            System.out.println("Nome completo: " + br.readLine() + " " + br2.readLine());
+
+        } catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
